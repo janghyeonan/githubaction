dd93f48ea12e5ee2b0797f8123582b35836cc1d4
janghyeonan <ajh910@gmail.com>
2022-03-15 11:14:37 +0900

    edit
    edit

--- autotest.py
+++ autotest.py
@@ -4,5 +4,5 @@ from bs4 import BeautifulSoup
 response = requests.get("https://www.clien.net/service/board/park")
 soup = BeautifulSoup(response.text, 'html.parser')
 print(datetime.datetime.now())
-for j in [ i.get_text().replace('\t',"").replace('\n',"") for i in soup.select('.subject_fixed') ]:
-    print(j)
+for j, k in zip([i.get_text().replace('\t',"").replace('\n',"") for i in soup.select('.subject_fixed')], [a.get_text().replace('\t',"").replace('\n',"") for a in soup.select('.timestamp')]):
+    print(k, j)

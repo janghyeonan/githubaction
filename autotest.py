750b005f4bc17aae9edc741321cec11da8f43be9
janghyeonan <ajh910@gmail.com>
2022-03-15 10:34:35 +0900

    add datetime
    add datetime

--- autotest.py
+++ autotest.py
@@ -1,6 +1,8 @@
 import requests
+import datetime
 from bs4 import BeautifulSoup
 response = requests.get("https://www.clien.net/service/board/park")
 soup = BeautifulSoup(response.text, 'html.parser')
+print(datetime.datetime.now())
 for j in [ i.get_text().replace('\t',"").replace('\n',"") for i in soup.select('.subject_fixed') ]:
     print(j)

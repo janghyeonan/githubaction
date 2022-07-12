2c2cf634625022eda97a46d7359e0ca5ed71e549
janghyeonan <ajh910@gmail.com>
2022-07-12 18:42:35 +0900

    uu
    uu

--- autotest.py
+++ autotest.py
@@ -2,5 +2,3 @@ import requests, telegram, sys, os
 response = requests.get("https://www.clien.net/service/board/cm_iphonien")
 if response.text.find(str(sys.argv[3])) != -1:    
     telegram.Bot(token=sys.argv[1]).send_message(chat_id=sys.argv[2], text='Clien Right now!!')
-else:
-    telegram.Bot(token=sys.argv[1]).send_message(chat_id=sys.argv[2], text='No~~!')
